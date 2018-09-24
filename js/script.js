5f72f328217957adaa9770525544899df3be53ef
avv-spirit <Sp1rit911@mail.ru>
2018-09-24 16:38:49 +0300

    Добавил действие JS при ошибке

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
 var buttonFind = document.querySelector(".button-find-hotels");
-var buttonAnimate = document.querySelector(".fade-in");
+var formAnimate = document.querySelector(".calendar-hotel");
+
+var form = document.querySelector("form");
+var arrivals = document.querySelector("[name=arrivals]")
+var departure = document.querySelector("[name=departure]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("arrivals");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 buttonFind.addEventListener("click", function (evt) {
   evt.preventDefault();
-  buttonAnimate.classList.toggle("fade-in");
-})
+  formAnimate.classList.toggle("fade-in");
+
+  if (storage) {
+    arrivals.value = storage
+    departure.focus();
+  } else {
+    arrivals.focus();
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivals.value || !departure.value) {
+    evt.preventDefault();
+    document.classList.add("form-error");
+    document.offsetWidth = document.offsetWidth;
+    document.classList.remove("form-error");
+  } else {
+    if (isStorageSupport)
+    localStorage.setItem("arrivals", arrivals.value);
+  }
+
+});
