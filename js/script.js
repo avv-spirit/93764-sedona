bb9f190623336883e531577e8cc1e84fe2877bc5
avv-spirit <Sp1rit911@mail.ru>
2018-09-24 12:59:22 +0300

    Новая анимация

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 var buttonFind = document.querySelector(".button-find-hotels");
 var buttonOff = document.querySelector(".calendar-hotel-off");
+var buttonAnimate = document.querySelector(".slide-down");
 
 buttonFind.addEventListener("click", function (evt) {
   evt.preventDefault();
   buttonOff.classList.toggle("calendar-hotel-off");
+  buttonAnimate.classList.toggle("slide-down");
 })
