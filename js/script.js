b6669025559d2753b50b93ad79d67e3ed9c39f5c
avv-spirit <Sp1rit911@mail.ru>
2018-09-24 14:45:47 +0300

    Готовая анимация, без "ошибки"

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 var buttonFind = document.querySelector(".button-find-hotels");
-var buttonOff = document.querySelector(".calendar-hotel-off");
-var buttonAnimate = document.querySelector(".slide-down");
+var buttonAnimate = document.querySelector(".fade-in");
 
 buttonFind.addEventListener("click", function (evt) {
   evt.preventDefault();
-  buttonOff.classList.toggle("calendar-hotel-off");
-  buttonAnimate.classList.toggle("slide-down");
+  buttonAnimate.classList.toggle("fade-in");
 })
